6c8840d86e03dc0b903cf94f6a5ef8f731cb88b8
Anas-elbourkkadi <elbourkkadianas@gmail.com>
2026-02-11 11:42:58 +0100

    added the navbar

---
+++ src/layout/header.jsx
@@ -0,0 +1,35 @@
+
+import { Link } from "react-router-dom";
+import { IoPersonOutline } from "react-icons/io5";
+import { PiShoppingBagLight } from "react-icons/pi";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+
+
+export const Header = () => {
+    return (
+        <div className="px-10 shadow-lg">
+            <div className="flex justify-between py-4 items-center">
+                <h1 className="font-black text-4xl"><Link>fashe<span className="text-red-600">.</span></Link></h1>
+                <ul className="lg:flex gap-10 hidden font-thin">
+                    <li className="border-b border-transparent hover:border-black"><Link>home</Link></li>
+                    <li className="border-b border-transparent hover:border-black"><Link>shop</Link></li>
+                    <li className="border-b border-transparent hover:border-black"><Link>sale</Link></li>
+                    <li className="border-b border-transparent hover:border-black"><Link>Features</Link></li>
+                    <li className="border-b border-transparent hover:border-black"><Link>Blog</Link></li>
+                    <li className="border-b border-transparent hover:border-black"><Link>About</Link></li>
+                    <li className="border-b border-transparent hover:border-black"><Link>Contact</Link></li>
+                </ul>
+                <div className="flex space-x-5 items-center font-thin">
+                    <div><IoPersonOutline strokeWidth={1} size={25}/></div>
+                    <span className="lg:inline hidden">|</span>
+                    <PiShoppingBagLight  size={25} />
+                    
+                    <GiHamburgerMenu className="lg:hidden cursor-pointer " size={25} />
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
